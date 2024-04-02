e5d9717632ae2ddd18d216b3605807982205d0af
Himanshuxyzz <himanshuxyzz@gmail.com>
2024-04-03 01:50:58 +0530

    chores: cart page ❌

--- Task-03-React/src/pages/Cart/index.jsx
+++ Task-03-React/src/pages/Cart/index.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "../../components/Header";
+import { useRef } from "react";
 
 const Cart = () => {
+  const Div = useRef(null);
+
+  useEffect(() => {
+    let a = document.querySelector(".cart-page");
+    console.log(a);
+  }, []);
+
   return (
     <>
       <NavBar />
-      <div className="cart-page"></div>
+      <div ref={Div} className="cart-page-container">
+        <section className=""></section>
+      </div>
     </>
   );
 };
